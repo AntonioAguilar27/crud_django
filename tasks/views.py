b1933f9b0d91b685a28c0583b25e51b328d880d7
Antonio <antonioaguilar_21@hotmail.com>
2025-01-29 16:48:15 -0700

    se termino la funcionalidad, queda pendiente terminar de estilziar

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -40,12 +42,17 @@ def signup(request):
             {"form": UserCreationForm, "error": "Password do not match"},
         )
 
-
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, "tasks.html", {"tasks": tasks})
 
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, "tasks.html", {"tasks": tasks})
 
+@login_required
 def create_task(request):
     if request.method == "GET":
         return render(request, "create_task.html", {"form": TaskForm})
@@ -63,13 +70,44 @@ def create_task(request):
                 {"form": TaskForm, "error": "please provide valid data"},
             )
 
-
+@login_required
 def task_detail(request, task_id):
-    task = get_object_or_404(Task, pk=task_id)
-    return render(request, "task_detail.html", {"task": task})
+    if request.method == "GET":
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, "task_detail.html", {"task": task, "form": form})
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect("tasks")
+        except ValueError:
+            return render(
+                request,
+                "task_detail.html",
+                {"task": task, "form": form, "error": "Error updating task"},
+            )
+
+@login_required
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
 
 
+@login_required
 def signout(request):
+    
     logout(request)
     return redirect("home")
 
@@ -94,4 +132,4 @@ def signin(request):
             )
         else:
             login(request, user)
-            return redirect("tasks")
+            return redirect("home")
